0ed748114352b84cfd97e5229cf631779c5d0ca9
krisnaItsAVirus <krisna+git@itsavirus.com>
2023-06-09 20:39:09 +0800

    Clean code

--- src/api/project/controllers/project.js
+++ src/api/project/controllers/project.js
@@ -1,46 +1,46 @@
-"use strict";
+'use strict'
 
 /**
  * project controller
  */
 
-const { createCoreController } = require("@strapi/strapi").factories;
+const { createCoreController } = require('@strapi/strapi').factories
 
-module.exports = createCoreController("api::project.project", ({ strapi }) => ({
+module.exports = createCoreController('api::project.project', ({ strapi }) => ({
   async find(ctx) {
-    const { query } = ctx;
+    const { query } = ctx
 
-    const entity = await strapi.entityService.findMany("api::project.project", {
+    const entity = await strapi.entityService.findMany('api::project.project', {
       ...query,
-      populate: ["thumbnail", "galery"],
-    });
+      populate: ['thumbnail', 'galery'],
+    })
 
-    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx)
 
-    return this.transformResponse(sanitizedEntity);
+    return this.transformResponse(sanitizedEntity)
   },
 
   async findOne(ctx) {
-    const { id: slug } = ctx.params;
-    const { query } = ctx;
+    const { id: slug } = ctx.params
+    const { query } = ctx
 
-    if (!query.filters) query.filters = {};
-    query.filters.slug = { $eq: slug };
+    if (!query.filters) query.filters = {}
+    query.filters.slug = { $eq: slug }
 
-    const entity = await strapi.service("api::project.project").find({
+    const entity = await strapi.service('api::project.project').find({
       ...query,
       populate: {
         thumbnail: true,
       },
       withRelated: {
         images: (qb) => {
-          qb.columns("data");
+          qb.columns('data')
         },
       },
-    });
+    })
 
-    const { results } = await this.sanitizeOutput(entity, ctx);
+    const { results } = await this.sanitizeOutput(entity, ctx)
 
-    return this.transformResponse(results[0]);
+    return this.transformResponse(results[0])
   },
-}));
+}))
